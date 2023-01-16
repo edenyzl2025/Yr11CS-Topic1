ac823acc00150e3e473727c67af136fedb220210
Eden L <edenlim@2025-Lim-Eden.local>
2023-01-16 15:11:06 +0800

    Initial commit

---
+++ src/Jan16.java
@@ -0,0 +1,38 @@
+public class Jan16 {
+    public static void main(String[] args) {
+////        int myInt = 10;
+//        int myInt2 = 4;
+//        double myDouble = 2.5;
+//        char myChar = 'A';
+
+//        System.out.println(myInt/myDouble);
+//        System.out.println(myInt/myInt2);
+
+//        int myInt3 = (int)myDouble;
+//        System.out.println(myInt3);
+////        System.out.println(12/0);
+//        System.out.println(12.0/0);
+
+        double myDouble2 = 4.6;
+        double myDouble3 = 4.4;
+        double myDouble4 = 4.5;
+        char myChar = 'd';
+        int myInt = 66;
+
+        myDouble2 += 0.5;
+        int myInt2 = (int)myDouble2;
+//        System.out.println(myInt2);
+
+//        int myInt3 = (int)myChar;
+//        System.out.println(myInt3);
+
+        char myChar3 = (char)myInt;
+//        System.out.println(myChar3);
+
+        int myInt3 = 7;
+        System.out.println(myInt3++);
+        System.out.println(myInt3);
+
+        System.out.println(++myInt3);
+    }
+}
